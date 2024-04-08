8f5636ba6cf3e6497a8c93ee09b091d8d295ec18
Danny Le <danny88.ca@gmail.com>
2024-04-07 20:03:13 -0600

    first commit

---
+++ src/main/java/org/comit/spring/BookLoversKingdomApplication.java
@@ -0,0 +1,13 @@
+package org.comit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLoversKingdomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLoversKingdomApplication.class, args);
+	}
+
+}
